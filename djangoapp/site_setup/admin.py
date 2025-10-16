b2263ad8f8d5112f73f1b0e9f8629cf76f80fe35
A-jota07 <souzajunioralexandre340@gmail.com>
2025-10-16 09:32:35 -0400

    Adicionando list_display e search_fields para acessar os links direcionados.

--- djangoapp/site_setup/admin.py
+++ djangoapp/site_setup/admin.py
@@ -4,3 +4,5 @@ from site_setup.models import MenuLink
 @admin.register(MenuLink)
 class MenulinkAdmin(admin.ModelAdmin):
     list_display = 'id','text', 'url_or_path',
+    list_display_links = 'id','text', 'url_or_path',
+    search_fiels = 'id','text', 'url_or_path',
