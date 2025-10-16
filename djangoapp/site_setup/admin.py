2e2925087ca9d30551ce989ba5f080a04320c248
A-jota07 <souzajunioralexandre340@gmail.com>
2025-10-16 09:28:58 -0400

    Adicionando a aba menu na area administrativa do django.

--- djangoapp/site_setup/admin.py
+++ djangoapp/site_setup/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from site_setup.models import MenuLink
 
-# Register your models here.
+@admin.register(MenuLink)
+class MenulinkAdmin(admin.ModelAdmin):
+    list_display = 'id','text', 'url_or_path',
