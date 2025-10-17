b14aa77338763feba1f49f394a1407a9605ef35e
A-jota07 <souzajunioralexandre340@gmail.com>
2025-10-17 10:13:12 -0400

    Adicionando validação de imagem

--- djangoapp/utils/model_validators.py
+++ djangoapp/utils/model_validators.py
@@ -5,3 +5,5 @@ from django.core.exceptions import ValidationError
 def validate_png(image):
     if not image.name.lower().endswith('.png'):
         raise ValidationError('Imagem precisa ser PNG')
+    
+    
