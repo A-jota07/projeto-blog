8a52ae275542b1229a0983eb51cb0fa82ebc4da9
A-jota07 <souzajunioralexandre340@gmail.com>
2025-10-27 10:24:13 -0400

    Criando os titulos das paginas e fazendo o tratamento

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,7 +1,9 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render
-from blog.models import Post, Page
+from blog.models import Post, Page, Tag
 from django.db.models import Q
+from django.contrib.auth.models import User
+from django.http import Http404
 
 
 PER_PAGE = 9
@@ -19,11 +21,23 @@ def index(request):
         'blog/pages/index.html',
         {
             'page_obj': page_obj,
+            'page_title': 'Home - ',
         }
     )
 
 def created_by(request, author_pk):
+    user = User.objects.filter(pk=author_pk).first()
+
+    if user is None:
+        raise Http404()
+
     posts = Post.objects.get_published().filter(created_by__pk=author_pk)
+    user_full_name = user.username
+
+    if user.first_name:
+        user_full_name = f'{user.first_name} {user.last_name}'
+    page_title = 'Posts de ' + user_full_name + ' - '
+        
     
     paginator = Paginator(posts, PER_PAGE)
     page_number = request.GET.get("page")
@@ -34,21 +48,29 @@ def created_by(request, author_pk):
         'blog/pages/index.html',
         {
             'page_obj': page_obj,
+            'page_title': page_title,
         }
     )
 
 def category(request, slug):
     posts = Post.objects.get_published().filter(category__slug=slug)
     
+    
     paginator = Paginator(posts, PER_PAGE)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
+    
+    if len(page_obj) == 0:
+        raise Http404()
+    
+    page_title = f'{page_obj[0].category.name} - Categoria - '
 
     return render(
         request,
         'blog/pages/index.html',
         {
             'page_obj': page_obj,
+            'page_title': page_title,
         }
     )
 
@@ -58,12 +80,21 @@ def tag(request, slug):
     paginator = Paginator(posts, PER_PAGE)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
+    
+    if len(page_obj) == 0:
+        raise Http404()
+
+    tag_obj = Tag.objects.filter(slug=slug).first()
+    tag_name = tag_obj.name if tag_obj else slug
+
+    page_title = f'{tag_name} - Tag - '
 
     return render(
         request,
         'blog/pages/index.html',
         {
             'page_obj': page_obj,
+            'page_title': page_title,
         }
     )
 
@@ -72,10 +103,12 @@ def search(request):
     
     posts = (Post.objects.get_published().filter(
             Q(title__icontains=search_value) | 
-            Q(excert__icontains=search_value) | 
+            Q(excerpt__icontains=search_value) | 
             Q(content__icontains=search_value)
         )[:PER_PAGE]
     )
+    
+    page_title = f'{search_value[:30]} - Search - '
 
     return render(
         request,
@@ -83,30 +116,42 @@ def search(request):
         {
             'page_obj': posts,
             'search_value': search_value,
+            'page_title': page_title,
         }
     )
 
 
 def page(request, slug):
-    page = Page.objects.filter(is_published=True).filter(slug=slug).first()
+    page_obj = Page.objects.filter(is_published=True).filter(slug=slug).first()
+    
+    if page_obj is None:
+        raise Http404()
+    
+    page_title = f'{page_obj.title} - Página - '
     
     return render(
         request,
         'blog/pages/page.html',
         {
-         'page': page,
+         'page': page_obj,
+         'page_title': page_title,
         }
     )
 
 
 def post(request, slug):
-    post = Post.objects.get_published().filter(slug=slug).first()
+    post_obj = Post.objects.get_published().filter(slug=slug).first()
 
+    if post_obj is None:
+        raise Http404()
+    
+    page_title = f'{post_obj.title} - Página - '
     
     return render(
         request,
         'blog/pages/post.html',
         {
-            'post': post,
+            'post': post_obj,
+            'page_title': page_title,
         }
     )
