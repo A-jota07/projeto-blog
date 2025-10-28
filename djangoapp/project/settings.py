516042d55e0c54b42022ad8cc7d49f48adfc3f90
A-jota07 <souzajunioralexandre340@gmail.com>
2025-10-28 10:25:56 -0400

    Adicionando DOTENV no settings e wsgi para segurança na admin. Configurando o django-axes na settings  para mais segurança.

--- djangoapp/project/settings.py
+++ djangoapp/project/settings.py
@@ -1,9 +1,15 @@
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 DATA_DIR = BASE_DIR.parent / 'data' / 'web'
+
+# DOTENV
+load_dotenv(BASE_DIR.parent / 'dotenv_files' / '.env', override=True)
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', 'change-me')
